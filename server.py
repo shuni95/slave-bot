0c631bfaf813436bb3a202dbe574fc44811f12f8
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-04-12 11:12:00 -0500

    change telegram :v

--- server.py
+++ server.py
@@ -1,19 +1,30 @@
-from flask import Flask, request
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from telegram.ext import Updater, CommandHandler
+from emoji import emojize
+import pymysql.cursors
+import logging
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                              user='root',
+                              password='root',
+                              db='slave',
+                              charset='utf8mb4',
+                              cursorclass=pymysql.cursors.DictCursor)
 
-app = Flask(__name__)
+logging.basicConfig(level=logging.INFO,
+                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-@app.route('/messenger/webhook', methods=['GET'])
-def handle_verification():
-    return request.args['hub.challenge']
+logger = logging.getLogger()
 
-@app.route('/messenger/webhook', methods=['POST'])
-def handle_incoming_requests():
-    data = request.json
-    messaging = data['entry'][0]['messaging'][0]
-    chat_id = messaging['sender']['id']
-    handler = BotHandler(chat_id)
+TOKEN='#'
+SITE_URL='#'
 
-    return handler.handle_bot(messaging)
+def start(bot, update):
+    update.message.reply_text('Hola!, Soy SlaveBot ' + emojize(':grinning_face:', use_aliases=True))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+updater = Updater(TOKEN)
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.bot.setWebhook(SITE_URL)
+updater.start_webhook(port=5000)
+updater.idle()

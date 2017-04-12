cdbd11342f29c9b54f503843e3bb1fc0e1faaadf
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-04-12 12:32:34 -0500

    add group support

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from telegram.ext import Updater, CommandHandler
 from emoji import emojize
 from user import User
+from group import Group
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
@@ -14,16 +15,27 @@ TOKEN='#'
 SITE_URL='#'
 
 def start(bot, update):
-    update.message.reply_text('Hola!, Soy SlaveBot ' + emojize(':grinning_face:', use_aliases=True))
+    update.message.reply_text(u'Hola!, Soy SlaveBot {}'.format(
+        emojize(':grinning_face:', use_aliases=True)))
 
 def create(bot, update):
+    _from = update.message.from_user
     chat = update.message.chat
-    user = User.find(chat.id)
-    if user is None:
-        User.create(chat)
-        update.message.reply_text(u'Hola {}, mucho gusto {}'.format(chat.first_name, emojize(':grinning_face:', use_aliases=True)))
-    else:
-        update.message.reply_text('Hola de nuevo {}'.format(user['name']))
+
+    if 'group' in chat['type']:
+        group = Group.find(chat['id'])
+        if group is None:
+            Group.create(chat)
+            update.message.reply_text('Hola grupo {}'.format(chat['title']))
+
+        user = User.find(_from['id'])
+        if user is None:
+            User.create(_from)
+            update.message.reply_text(u'Hola {}, mucho gusto {}'.format(
+                chat.first_name,
+                emojize(':grinning_face:', use_aliases=True)))
+        else:
+            update.message.reply_text('Hola de nuevo {}'.format(user['name']))
 
 updater = Updater(TOKEN)
 updater.dispatcher.add_handler(CommandHandler('start', start))
